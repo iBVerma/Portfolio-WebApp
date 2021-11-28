9477cb100e0d5e7f0531e178370e38aa846f815f
Brijesh Verma <74365435+iBVerma@users.noreply.github.com>
2021-11-28 17:17:52 +0530

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Me from './Me';
 import Projects from './Projects';
-import Work from './Work';
 import Education from './Education';
 
 const Pages = ({ user }) => {
@@ -14,9 +13,6 @@ const Pages = ({ user }) => {
         </Route>
         <Route path="/projects">
           <Projects user={user} />
-        </Route>
-        <Route path="/work">
-          <Work user={user} />
         </Route>
         <Route path="/education">
           <Education user={user} />
